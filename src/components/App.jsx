e04269344b74ec07502246fb896dd181882031c8
IngraIssdottir <ingra.issdottir@yahoo.es>
2025-11-24 12:08:53 +0100

    creating preview component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,8 +6,10 @@ import LogoAdalab from "../images/adalab.png";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 import Hero from "./layout/Hero";
+import Preview from "./create/Preview";
 
 import { useState } from "react";
+
 
 function App() {
 
@@ -64,48 +66,7 @@ function App() {
         
       <Hero />
 
-        <section className="preview">
-          <div className="projectImage"></div>
-          <article className="card">
-            <h2 className="card__projectTitle">
-              <span className="card__projectTitle--text">
-                Personal project card
-              </span>
-            </h2>
-
-            <div className="card__author">
-              <div className="card__authorPhoto"></div>
-              <p className="card__job">{job === '' ? 'Full stack Developer' : job}</p>
-              <h3 className="card__name">{author === '' ? 'Emmelie Bjôrklund' : author}</h3>
-            </div>
-
-            <div className="card__project">
-              <h3 className="card__name">{name === '' ? 'Elegant Workspace' : name}</h3>
-              <p className="card__slogan">{slogan === '' ? 'Diseños Exclusivos' : slogan}</p>
-              <h3 className="card__descriptionTitle">Product description</h3>
-              <p className="card__description">{desc === '' ? 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, quos? Itaque, molestias eveniet laudantium adipisci vitae ratione' : desc}</p>
-
-              <div className="card__technicalInfo">
-                <p className="card__technologies">{technologies === '' ? 'React JS - HTML - CSS' : technologies}</p>
-
-                <a
-                  className="icon icon__www"
-                  href={demo}
-                  title="Haz click para ver el proyecto online"
-                >
-                  {demo}
-                </a>
-                <a
-                  className="icon icon__github"
-                  href={repo}
-                  title="Haz click para ver el código del proyecto"
-                >
-                  {repo}
-                </a>
-              </div>
-            </div>
-          </article>
-        </section>
+        <Preview name={name} slogan={slogan} repo={repo} demo={demo} technologies={technologies} desc={desc} author={author} job={job} />
         <form className="addForm">
           <h2 className="title">Información</h2>
           <fieldset className="addForm__group">
