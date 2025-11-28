4979f44819d62c79c8076c6a53dd370888026094
Monica Coca <mcocapelaz@gmail.com>
2025-11-28 13:31:21 +0100

    fetch and local storage added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,27 +6,58 @@ import Footer from "./layout/Footer";
 import Hero from "./layout/Hero";
 import Preview from "./create/Preview";
 import Form from "./create/Form";
-import { useState } from "react";
+import { useState, useEffect} from "react";
+
 
 function App() {
   const [cardData, setCardData] = useState({
-    name: '',
-    slogan: '',
-    repo: '',
-    demo: '',
-    technologies: '',
-    desc: '',
-    author: '',
-    job: '',
-    photo: '',
-    image: '',
+    name: "",
+    slogan: "",
+    repo: "",
+    demo: "",
+    technologies: "",
+    desc: "",
+    author: "",
+    job: "",
+    photo: "",
+    image: "",
   });
+
+ useEffect(() => {
+  // Este código se lanza cuando cambie cardData
+  localStorage.setItem('form-backup', JSON.stringify(cardData));
+}, [cardData]);
 
   const handleInputCard = (ev) => {
     setCardData({
       ...cardData,
-      [ev.target.name]: ev.target.value
+      [ev.target.name]: ev.target.value,
     });
+  };
+
+  const handleClick = () => {
+    fetch("https://dev.adalab.es/api/projectCard", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(cardData),
+    })
+      .then((res) => res.json())
+      .then((responseData) => {
+        if (responseData.success) {
+          // Hacemos algo para indicar la dirección del servidor, ole y ole
+        } else {
+          // Hacemos algo para indicar que hay un error, fíjate tú
+        }
+      })
+      .catch((error) => {
+        console.error("Error en la petición:", error);
+      });
+  };
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
   };
 
   return (
@@ -35,7 +66,11 @@ function App() {
       <main className="main">
         <Hero />
         <Preview cardData={cardData} />
-        <Form handleInputCard={handleInputCard} cardData={cardData} setCardData={setCardData} />
+        <Form
+          handleInputCard={handleInputCard}
+          cardData={cardData}
+          setCardData={setCardData}
+        />
       </main>
       <Footer />
     </div>
